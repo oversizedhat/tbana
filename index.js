d36916939699f560c4ead787815e00d259e62cae
Oscar Berg <oscar@oscarberg.com>
2019-10-31 20:34:52 +0100

    add some error handling and retries

--- index.js
+++ index.js
@@ -1,53 +1,117 @@
 'use strict'
 const http = require('http');
-const AWS = require('aws-sdk');
 
+const TRAFIKLAB_API_ENDPOINT = "http://api.sl.se/api2/realtimedeparturesV4.json";
 const DEFAULT_SITE_ID = 9141; //bagarmossen
-const JOURNEY_DIRECTION_NORTH = 1; //from bagarmossen towards sthlm central that is
-
-const S3_BUCKET = "tbana-db";
 const TRAFIKLAB_API_KEY = process.env.TRAFIKLAB_API_KEY;
+const TRAFIKLAB_MAX_RETRIES = 5; // this includes the initial request. ie we try a total of MAX_RETRIES times.
+const TRAFIKLAB_RETRY_INTERVAL_MS = 2000;
 
 const allowedOrigins = ["http://localhost:8080","http://www.coommuter.com"];
 
-//lambda handler
-async function handler(event) { 
+/**
+ * OK response:
+ * 200 - Valid response with json body with train info
+ * 
+ * Error responses:
+ * 400 - Invalid url parameters
+ * 403 - Forbidden, likely due to CORS
+ * 503 - Failed fetching info from trafiklab service
+ * 
+ * ------- 
+ * 
+ * Cut from Trafiklab API docs
+ * https://www.trafiklab.se/api/sl-realtidsinformation-4/sl-realtidsinformation-4
+ *
+ * StatusCode Integer Innehåller statuskod för det eventuella meddelandet.
+ * Message String Innehåller eventuellt anropsrelaterade meddelanden som t.ex. felmeddelanden. Se ”Felmeddelanden” nedan.
+ * ExecutionTime Long Anger hur lång tid (i ms) det tog för servern att generera svaret.
+ * ResponseData Departure Innehåller själva svarsdata från tjänsten. Se ”Svarsdata” nedan.
+ * 
+ * Error codes:
+ * 1001 problem with request: Key is undefined Nyckel hare ej skickats med.
+ * 1002 problem with request: Key is invalid Nyckel är ogiltig
+ * 1003 Invalid api Ogiltigt api
+ * 1004 problem with request: This api is currently not available for keys with priority above 2
+ * 1005 Nyckel finns, men ej för detta api problem with request: Invalid api for key
+ * 1006 To many requests per minute
+ * 1007 To many requests per month
+ * 4001 SiteId måste gå att konvertera till heltal.
+ * 5321 Kunde varken hämta information från TPI (tunnelbanan) eller DPS (övriga trafikslag).
+ * 5322 Kunde inte hämta information från DPS.
+ * 5323 Kunde inte hämta information från TPI.
+ * 5324 Kunde varken hämta information från TPI (tunnelbanan) eller DPS (övriga trafikslag) p.g.a. inaktuell DPS-data. Detta uppstår om DPS-datan är äldre än 2 minuter vid svarstillfället.
+ *
+ * 
+ * TODO validate that the requested siteid exists
+ */
+
+async function lambdaHandler(event) { 
     const origin = event.headers && event.headers.origin;
-    if (!origin || !allowedOrigins.includes(origin)){
+    if (!origin || !allowedOrigins.includes(origin)) {
         return {
-          statusCode: 403,
-          headers: {
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": allowedOrigins[0]
-          }
+            statusCode: 403,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': allowedOrigins[0]
+            }
         }
     }
-    
-    const siteId = (event.queryStringParameters && event.queryStringParameters.siteId)?event.queryStringParameters.siteId:DEFAULT_SITE_ID;
-    const dir = (event.queryStringParameters && event.queryStringParameters.dir)?event.queryStringParameters.dir:JOURNEY_DIRECTION_NORTH;
 
-    //fetch train info from trafiklab 
-    const trains = (await fetchRealtimeDepatures(siteId)).Metros.filter((train) => {
-        return train.JourneyDirection == dir
-    });
-  
-    //Note CORS reply with same origin
+    const siteId = (event.queryStringParameters && event.queryStringParameters.siteId)?event.queryStringParameters.siteId:undefined;
+    const dir = (event.queryStringParameters && event.queryStringParameters.dir)?event.queryStringParameters.dir:undefined;
+
+    // Prep response, note CORS reply with same origin
     const response = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": origin
-      },
-      body: JSON.stringify(trains)
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": origin
+        }
+    }
+
+    if (!siteId || Number.isNaN(parseInt(siteId))) {
+        response.statusCode = 400;
+        response.body = "siteId (integer) required as get parameter.";
+    } else if (!dir || Number.isNaN(parseInt(dir))) {
+        response.statusCode = 400;
+        response.body = "dir (integer) required as get parameter, specifying direction of travel from the given site.";
+    } else {
+        // Fetch train info from trafiklab on given site, filtered using dir.
+        // As the service is a bit unreliable we try a couple of times..
+        try {
+            const sleep = async (ms) => {
+                return new Promise(resolve => setTimeout(resolve, ms))
+            }
+            const retry = async (siteId, n) => {
+                try {
+                    return (await fetchRealtimeDepatures(siteId)).Metros.filter((train) => {
+                        return train.JourneyDirection == dir
+                    });
+                } catch(err) {
+                    if (n === 1) {
+                        throw err;
+                    }
+                    await sleep(TRAFIKLAB_RETRY_INTERVAL_MS);
+                    return await retry(siteId, n - 1);
+                }
+            };
+
+            const trains = await retry(siteId, TRAFIKLAB_MAX_RETRIES);
+            response.statusCode = 200;
+            response.body = JSON.stringify(trains);
+        } catch (e) {
+            response.statusCode = 503;
+            response.body = JSON.stringify(e);
+        }   
     }
     return response;
 }
 
-//fetch data using trafiklab service
+// Fetch data using trafiklab service
 async function fetchRealtimeDepatures(siteId = DEFAULT_SITE_ID, timeWindow = 60, bus = false) {
     return new Promise((resolve, reject) => {
-        const url = "http://api.sl.se/api2/realtimedeparturesV4.json?key=" + TRAFIKLAB_API_KEY + "&siteid=" + siteId + "&timewindow=" + timeWindow + "&Bus=" + bus;
-
+        const url = `${TRAFIKLAB_API_ENDPOINT}?key=${TRAFIKLAB_API_KEY}&siteid=${siteId}&timewindow=${timeWindow}&Bus=${bus}`;
+        console.log(`get ${url}`);
         http.get(url, (res) => {
             res.setEncoding('utf8');
             let rawData = '';
@@ -55,34 +119,20 @@ async function fetchRealtimeDepatures(siteId = DEFAULT_SITE_ID, timeWindow = 60,
             res.on('end', () => {
                 try {
                     const parsedData = JSON.parse(rawData);
-                    //TODO handle error codes (parsedData.StatusCode): https://www.trafiklab.se/api/sl-realtidsinformation-4/sl-realtidsinformation-4
-                    resolve(parsedData.ResponseData);
+                    if (!parsedData.ResponseData) {
+                        reject(new Error(`Trafiklab request failed with ${parsedData.StatusCode} - ${parsedData.Message}`));
+                    } else {
+                        resolve(parsedData.ResponseData);
+                    }
                 } catch (e) {
-                    console.error(e.message);
+                    // Unable to parse json
+                    reject(e);
                 }
             });
         });
     });
 }
 
-//will bucket even be needed?
-async function loadDBFromBucket() {
-	return new Promise((resolve, reject) => {
-		var s3 = new AWS.S3();
-		s3.getObject({
-			Bucket: S3_BUCKET, Key: "db.json"
-			},
-			function (err, data) {
-				if (err != null) {
-					reject([503, "Failed to load data from DB"]);
-				} else {
-					resolve(JSON.parse(data.Body.toString()));
-				}
-			}
-		);
-	});
-}
-
 module.exports = {
-    handler: handler
+    handler: lambdaHandler
 }
--- test/integration.js
+++ test/integration.js
@@ -1,29 +1,45 @@
 let assert = require('assert');
 let lib = require('../index.js');
 
-const MOCK_HEADER_OK = { origin: "http://www.localhost:8080" };
-const MOCK_HEADER_NOT_OK = { origin: "http://test.test.test" };
-const DEFAULT_EVENT = { headers : MOCK_HEADER_OK};
+const MOCK_HEADER_OK = { origin: "http://localhost:8080" };
+const MOCK_HEADER_NOT_OK = { origin: "http://not.ok.domain" };
+const VALID_PARAMS = { siteId: 9141, dir: 1 }
+const DEFAULT_EVENT = { headers : MOCK_HEADER_OK, queryStringParameters: VALID_PARAMS};
 /**
- * Some kind of e2e tests here, actually using the trafiklab service
+ * Some kind of e2e integratin tests here, even using the trafiklab service
  */
 describe('Validate json', function() {
-    it('should respond with 200', async function() {
-        this.timeout(5000);
-        const response = await lib.handler(DEFAULT_EVENT);
-        assert.equal(response.statusCode, 200);
-    });
-
-    it('should respond with valid json data', async function() {
+    it('should respond with valid json data on a valid request', async function() {
         this.timeout(5000);
         const response = await lib.handler(DEFAULT_EVENT);
         const parsedData = JSON.parse(response.body);
+
+        assert.equal(response.statusCode, 200);
         assert.ok(parsedData);
     });
 
     it('should not allow unknown domain', async function() {
-        this.timeout(5000);
         const response = await lib.handler({headers:MOCK_HEADER_NOT_OK});
         assert.equal(response.statusCode, 403);
+    });
+
+    it('should reject request without siteId as url parameter', async function() {
+        const response = await lib.handler({headers:MOCK_HEADER_OK, queryStringParameters:{dir:1}});
+        assert.equal(response.statusCode, 400);
+    });
+
+    it('should reject request without dir as url parameter', async function() {
+        const response = await lib.handler({headers:MOCK_HEADER_OK, queryStringParameters:{siteId:4364}});
+        assert.equal(response.statusCode, 400);
+    });
+
+    it('should reject request with invalid siteId typing', async function() {
+        const response = await lib.handler({headers:MOCK_HEADER_OK, queryStringParameters:{siteId:"notint", dir:1}});
+        assert.equal(response.statusCode, 400);
+    });
+
+    it('should reject request without url parameters', async function() {
+        const response = await lib.handler({headers:MOCK_HEADER_OK});
+        assert.equal(response.statusCode, 400);
     });
 });
